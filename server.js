cf0b139f5450f32d607c522f3d1eec6660665b81
Shivendra Shukla <shivendrashukla06@gmail.com>
2025-07-29 18:16:19 +0530

    Added a client frontend built using React

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 const express = require("express");
-const socketIO = require("socket.io");
-const app = express();
 const cors = require("cors");
+const http = require("http");
+const socketIO = require("socket.io");
 
+const app = express();
 app.use(cors({ origin: "*" }));
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Signaling server is up!");
 });
 
-app.use((req, res, next) => {
-  //To allow cross origin resource sharing
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
-  );
-
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-const server = app.listen(3000, () => console.log("listening at port 3000"));
-
+const server = http.createServer(app);
 const io = socketIO(server, {
   cors: {
     origin: "*",
@@ -30,34 +18,64 @@ const io = socketIO(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  socket.on("join-room", (roomId) => {
-    socket.join(roomId);
-    console.log(`${socket.id} joined room ${roomId}`);
+const rooms = {}; // Keep track of users per room
 
-    const room = io.sockets.adapter.rooms.get(roomId);
-    if (room && room.size === 2) {
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    console.log(`${socket.id} is joining room ${roomId}`);
+    socket.join(roomId);
+
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+    rooms[roomId].push(socket.id);
+
+    // When two people are in the room, signal that both are ready
+    if (rooms[roomId].length === 2) {
+      console.log(`Room ${roomId} is full. Emitting 'ready'`);
       io.to(roomId).emit("ready");
     }
-  });
 
-  socket.on("offer", ({ offer, room }) => {
-    socket.to(room).emit("offer", { offer });
-  });
+    // Relay offer to others in the room
+    socket.on("offer", (data) => {
+      console.log("Offer received from", socket.id);
+      socket.to(roomId).emit("offer", data);
+    });
 
-  socket.on("answer", ({ answer, room }) => {
-    socket.to(room).emit("answer", { answer });
-  });
+    // Relay answer
+    socket.on("answer", (data) => {
+      console.log("Answer received from", socket.id);
+      socket.to(roomId).emit("answer", data);
+    });
 
-  socket.on("ice-candidate", ({ candidate, room }) => {
-    socket.to(room).emit("ice-candidate", { candidate });
-  });
+    // Relay ICE candidates
+    socket.on("ice-candidate", (data) => {
+      console.log("ICE candidate received from", socket.id);
+      socket.to(roomId).emit("ice-candidate", data);
+    });
 
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("leave");
-  });
+    // Relay chat messages
+    socket.on("chat message", (msg) => {
+      console.log("Chat message:", msg);
+      socket.to(roomId).emit("chat message", msg);
+    });
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+    // Handle disconnects
+    socket.on("disconnect", () => {
+      console.log(`${socket.id} disconnected from room ${roomId}`);
+      rooms[roomId] = rooms[roomId]?.filter((id) => id !== socket.id);
+      socket.to(roomId).emit("leave");
+
+      // Clean up empty rooms
+      if (rooms[roomId]?.length === 0) {
+        delete rooms[roomId];
+      }
+    });
   });
+});
+
+server.listen(3000, () => {
+  console.log("Signaling server listening on port 3000");
 });
